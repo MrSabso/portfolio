44add3645b4fc5a58dfb27259b1c9ecace3f6231
Saba <godmode7616@gmail.com>
2025-06-26 13:08:26 +0400

    creating portfolio

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        lightHover: "#fcf4ff",
+        darkHover: "#2a004a",
+        darkTheme: "#11001f",
+      },
+      fontFamily: {
+        outfit: ["Outfit", "sans-serif"],
+        ovo: ["Ovo", "serif"],
+      },
+    },
+  },
+  plugins: [],
+};
